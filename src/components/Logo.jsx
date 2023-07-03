732713dae100a6d3784a8241177c87c836e3d338
WNagorny <walery.nagorny@gmail.com>
2023-07-03 17:27:57 +0200

    added logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logoSvg from '../assets/logo.svg'
+
+const Logo = () => {
+	return (
+		<Link to='/' className='absolute top-[1.5rem] left-[3rem] text-xl text-amber-500 flex'>
+			<img src={logoSvg} alt='CoinVibe logo'  className='mr-3'/>
+         <span>CoinVibe</span>
+		</Link>
+	)
+}
+
+export default Logo
