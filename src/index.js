b6904bf9a9eed3d6b7063936e211676019df3687
WNagorny <walery.nagorny@gmail.com>
2023-07-03 16:21:25 +0200

    added router to the HomePage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import App from './App'
+
 import Home from './Pages/Home'
+import Crypto from './Pages/Crypto'
+import Trending from  './Pages/Trending'
+import Saved from  './Pages/Saved'
+
 
 const router = createBrowserRouter([
 	{
+    path: "/",
+    element: <Home/>,
+    children : [
+      {
+        path: "/",
+        element: <Crypto/>
+      },
+      {
+        path: "/trending",
+        element: <Trending/>
+      },
+      {
+        path: "/saved",
+        element: <Saved/>
+      },
+    ]
 	},
 ])
 
