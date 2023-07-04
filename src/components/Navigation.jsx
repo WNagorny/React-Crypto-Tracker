e5578332a218434119483ef9b9eb0c42ab991eb5
WNagorny <walery.nagorny@gmail.com>
2023-07-04 15:57:18 +0200

    added navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navigation = () => {
+	return (
+		<nav className='w-[40%] mt-16 flex justify-around align-middle border border-amber-500 rounded-lg'>
+			<NavLink
+				to='/'
+            end
+				className={({ isActive }) => {
+					return `w-full text-base text-center font-nunito m-2.5
+               ${
+									isActive
+										? 'bg-amber-500 text-gray-300'
+										: 'bg-gray-200 text-gray-100 hover:text-amber-500 active:bg-amber-500 active:text-gray-300'
+								}
+                 border-0 cursor-pointer rounded capitalize font-semibold`
+				}}
+			>
+				crypto
+			</NavLink>
+			<NavLink
+				to='/trending'
+				className={({ isActive }) => {
+					return `w-full text-base text-center font-nunito m-2.5
+               ${
+									isActive
+										? 'bg-amber-500 text-gray-300'
+										: 'bg-gray-200 text-gray-100 hover:text-amber-500 active:bg-amber-500 active:text-gray-300'
+								}
+                 border-0 cursor-pointer rounded capitalize font-semibold`
+				}}
+			>
+				trending
+			</NavLink>
+			<NavLink
+				to='/saved'
+				className={({ isActive }) => {
+					return `w-full text-base text-center font-nunito m-2.5
+               ${
+									isActive
+										? 'bg-amber-500 text-gray-300'
+										: 'bg-gray-200 text-gray-100 hover:text-amber-500 active:bg-amber-500 active:text-gray-300'
+								}
+                 border-0 cursor-pointer rounded capitalize font-semibold`
+				}}
+			>
+				saved
+			</NavLink>
+		</nav>
+	)
+}
+
+export default Navigation
