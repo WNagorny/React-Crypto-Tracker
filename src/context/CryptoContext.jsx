115f050af69d9c7203a09d8926094c73e68cfa69
WNagorny <walery.nagorny@gmail.com>
2023-07-04 17:20:14 +0200

    added context for table - API

---
+++ src/context/CryptoContext.jsx
@@ -0,0 +1,33 @@
+import { useLayoutEffect, createContext, useState } from 'react'
+
+
+//create context object
+export const CryptoContext = createContext({})
+
+//create the provider component
+export const CryptoProvider = ({ children }) => {
+	const [cryptoData, setCryptoData] = useState()
+
+	const getCryptodata = async () => {
+		try {
+			const data = await fetch(
+            
+				`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d&locale=en`
+			)
+				.then(res => res.json())
+				.then(json => json)
+
+			console.log(data)
+			setCryptoData(data)
+
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useLayoutEffect(() => {
+		getCryptodata()
+	}, [])
+
+	return <CryptoContext.Provider value={{cryptoData}}>{children}</CryptoContext.Provider>
+}
